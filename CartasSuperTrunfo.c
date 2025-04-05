b4eb363d661ebf7b37015466a3659553bf69b86b
nilsongpk <nilsongpk@gmail.com>
2025-04-05 22:11:56 +0000

    Erro Cidade/população

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,22 +12,22 @@ int Turisticos;
 
 
 printf("Digite Código da Carta: \n");
-scanf("%f", &Carta);
+scanf("%s", Carta);
+
+printf("Digite o Estado: \n");
+scanf("%s", Estado);  
+getchar();
+
+printf("Digite a Cidade \n");
+scanf("%s", Cidade);
 
 
+printf("Digite a População: \n");
+scanf("%d", &Populacao);
 
+printf("Digite Área (em km²): \n");
+scanf("%f", Area);
 
-    
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+return 0;
 
-    return 0;
 }
