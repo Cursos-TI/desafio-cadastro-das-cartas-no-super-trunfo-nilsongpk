fe695f06c1e5643504ffdabdad4dc57343e63a05
nilsongpk <nilsongpk@gmail.com>
2025-04-12 22:30:18 +0000

    DENSIDADE

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-
 int main() {
-char Carta1[10], Carta2[10];
-char Estado1[3], Estado2[3];
+ char Carta1[10], Carta2[10];
+ char Estado1[3], Estado2[3];
 char Cidade1[50], Cidade2[50];
-int Populacao1, Populacao2;
-float Area1, Area2;
-float Pib1, Pib2;
-int Turisticos1, Turisticos2;
+ int Populacao1, Populacao2;
+ float Area1, Area2;
+ float Pib1, Pib2;
+ int Turisticos1, Turisticos2;
+ float densidade1, densidade2;
 
 printf("======= Cadastro da Carta 1 =======\n");
 
@@ -61,26 +61,29 @@ scanf("%f", &Pib2);
 printf("Digite o Número de Pontos Turísticos da Cidade: \n");
 scanf("%d", &Turisticos2);
 
+densidade1 = (float)Populacao1 / Area1;
+densidade2 = (float)Populacao2 / Area2;
+
 printf("\n\n====== INFORMAÇÕES DAS CARTAS ======\n");
 
-printf("\nCarta 1:\n");
-printf("Estado: %s\n", Estado1);
-printf("Nome da Cidade: %s\n", Cidade1);
-printf("População: %d\n", Populacao1);
-printf("Aréa (em km²): %2.f\n", Area1);
-printf("PIB: %.2f bilhões de reais\n", Pib1);
-printf("Pontos Turísticos da Cidade: %d\n", Turisticos1);
+ printf("\nCarta 1:\n");
+ printf("Estado: %s\n", Estado1);
+ printf("Nome da Cidade: %s\n", Cidade1);
+ printf("População: %d\n", Populacao1);
+ printf("Área (em km²): %.2f\n", Area1);
+ printf("PIB: %.2f bilhões de reais\n", Pib1);
+ printf("Pontos Turísticos da Cidade: %d\n", Turisticos1);
+ printf("A Densidade Populacional é: %.2f hab/km²\n", densidade1);
+ printf("\nCarta 2:\n");
+ printf("Estado: %s\n", Estado2);
+ printf("Nome da Cidade: %s\n", Cidade2);
+ printf("População: %d\n", Populacao2);
+ printf("Área (em km²): %.2f\n", Area2);
+ printf("PIB: %.2f bilhões de reais\n", Pib2);
+ printf("Pontos Turísticos da Cidade: %d\n", Turisticos2);
+ printf("A Densidade Populacional é: %.2f hab/km²\n", densidade2);
 
-
-printf("\nCarta 2:\n");
-printf("Estado: %s\n", Estado2);
-printf("Nome da Cidade: %s\n", Cidade2);
-printf("População: %d\n", Populacao2);
-printf("Aréa (em km²): %2.f\n", Area2);
-printf("PIB: %.2f bilhões de reais\n", Pib2);
-printf("Pontos Turísticos da Cidade: %d\n", Turisticos2);
-
+ printf("\n\n==========================\n");
 
 return 0;
-
 }
